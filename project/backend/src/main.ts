a3597c0eaba904fd8214d0a94a7ce87fc5bc68e3
caonhathao <caonhathao2372004@gmail.com>
2026-02-14 10:38:36 +0700

    update: config and fix

--- project/backend/src/main.ts
+++ project/backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import 'tsconfig-paths/register';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
